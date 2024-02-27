85ab30a4c71c81756ef59ad0f00e631a20c44fbf
hanbing <hanbing@junohoro.com>
2024-02-27 14:49:18 +0800

    路由初始化

---
+++ bootstrap/route.go
@@ -0,0 +1,45 @@
+// Package bootstrap 处理程序初始化逻辑
+package bootstrap
+
+import (
+	"github.com/gin-gonic/gin"
+	"go_learn/routes"
+	"net/http"
+	"strings"
+)
+
+func SetupRoute(router *gin.Engine) {
+	// 注册全局中间件
+	registerGlobalMiddleWare(router)
+	// 注册API路由
+	routes.RegisterAPIRoutes(router)
+	// 配置404路由
+	setup404Handler(router)
+}
+
+func registerGlobalMiddleWare(router *gin.Engine) {
+	// 初始化  Default 初始化 = New() + 注册中间件
+	//r := gin.Default()
+	router.Use(
+		gin.Logger(),
+		gin.Recovery(),
+	)
+}
+
+func setup404Handler(router *gin.Engine) {
+	// 处理404
+	router.NoRoute(func(c *gin.Context) {
+		// 获取标头信息 Accept 信息
+		acceptString := c.Request.Header.Get("Accept")
+		if strings.Contains(acceptString, "text/html") {
+			// 如果是html
+			c.String(http.StatusNotFound, "页面返回 404")
+		} else {
+			// 默认返回JSON
+			c.JSON(http.StatusOK, gin.H{
+				"error_code":    404,
+				"error_message": "路由未定义， 请确认URL和请求方法是否正确",
+			})
+		}
+	})
+}
--- main.go
+++ main.go
@@ -1,47 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
-	"net/http"
-	"strings"
+	"go_learn/bootstrap"
 )
 
 func main() {
-	// 初始化  Default 初始化 = New() + 注册中间件
-	//r := gin.Default()
 
 	// 注册一个Gin Engine实例
 	r := gin.New()
 
-	// 注册中间件
-	r.Use(gin.Logger(), gin.Recovery())
-
-	// 注册一个路由
-	r.GET("/", func(c *gin.Context) {
-		// 以JSON格式响应
-		c.JSON(http.StatusOK, gin.H{
-			"Hello": "World",
-		})
-	})
-
-	// 404 请求
-	r.NoRoute(func(c *gin.Context) {
-		acceptString := c.Request.Header.Get("Accept")
-		if strings.Contains(acceptString, "text/html") {
-			// 如果是HTML的话
-			c.String(http.StatusNotFound, "页面返回 404")
-		} else {
-			// 默认返回JSON
-			c.JSON(http.StatusNotFound, gin.H{
-				"error_code":    404,
-				"error_message": "路由未定义",
-			})
-		}
-	})
+	// 初始化路由绑定
+	bootstrap.SetupRoute(r)
 
 	// 运行服务，指定8080端口
 	err := r.Run(":8000")
 	if err != nil {
-		return
+		// 错误处理
+		fmt.Println(err.Error())
 	}
 }
