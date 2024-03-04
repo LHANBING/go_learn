5d7a4091302d8175d59d66ee7be29c31103eefde
hanbing <hanbing@junohoro.com>
2024-03-04 15:03:41 +0800

    命令行模式

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/pkg/errors v0.9.1
 	github.com/redis/go-redis/v9 v9.5.1
 	github.com/spf13/cast v1.6.0
+	github.com/spf13/cobra v1.8.0
 	github.com/spf13/viper v1.18.2
 	github.com/thedevsaddam/govalidator v1.9.10
 	github.com/ulule/limiter/v3 v3.11.2
@@ -39,6 +40,7 @@ require (
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/google/uuid v1.4.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"github.com/gin-gonic/gin"
+	"go_learn/app/cmd"
 	"go_learn/bootstrap"
 	btsConfig "go_learn/config"
 	"go_learn/pkg/config"
+	"go_learn/pkg/console"
+	"os"
+
+	"github.com/spf13/cobra"
 )
 
 func init() {
@@ -15,9 +18,46 @@ func init() {
 }
 
 func main() {
+	// 应用的主入口，默认调用 cmd.CmdServe 命令
+	var rootCmd = &cobra.Command{
+		Use:   "GoLearn",
+		Short: "A simple forum project",
+		Long:  `Default will run "serve" command, you can use "-h" flag to see all subcommands`,
 
+		PersistentPreRun: func(command *cobra.Command, args []string) {
+			// 配置初始化，依赖命令行 --env参数
+			config.InitConfig(cmd.Env)
+
+			// 初始化 Logger
+			bootstrap.SetupLogger()
+
+			// 初始化数据库
+			bootstrap.SetupDB()
+
+			// 初始化 Redis
+			bootstrap.SetupRedis()
+		},
+	}
+
+	// 注册子命令
+	rootCmd.AddCommand(
+		cmd.CmdServe,
+	)
+
+	// 配置默认运行 Web 服务
+	cmd.RegisterDefaultCmd(rootCmd, cmd.CmdServe)
+
+	// 注册全局参数，--env
+	cmd.RegisterGlobalFlags(rootCmd)
+
+	// 执行命令
+	if err := rootCmd.Execute(); err != nil {
+		console.Exit(fmt.Sprintf("Failed to run app with %v:%s", os.Args, err.Error()))
+	}
+
+	// ------- old
 	// 配置初始化，依赖命令行 --env参数
-	var env string
+	/*var env string
 	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
 	flag.Parse()
 	config.InitConfig(env)
@@ -44,14 +84,14 @@ func main() {
 	bootstrap.SetupRoute(router)
 
 	// 测试路由
-	/*router.GET("/v1/test", middleware.GuestJWT(), func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello guest !")
-	})*/
+	//router.GET("/v1/test", middleware.GuestJWT(), func(c *gin.Context) {
+	//	c.String(http.StatusOK, "Hello guest !")
+	//})
 
 	// 运行服务，指定端口
 	err := router.Run(":" + config.Get("app.port"))
 	if err != nil {
 		// 错误处理
 		fmt.Println(err.Error())
-	}
+	}*/
 }
