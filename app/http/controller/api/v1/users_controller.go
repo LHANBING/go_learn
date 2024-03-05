728b52c94c3fd1af6b04c53ef9e5fbfc09c6eb22
hanbing <hanbing@junohoro.com>
2024-03-05 17:57:12 +0800

    验证分页请求

--- app/http/controller/api/v1/users_controller.go
+++ app/http/controller/api/v1/users_controller.go
@@ -2,6 +2,7 @@ package v1
 
 import (
 	"go_learn/app/models/user"
+	"go_learn/app/requests"
 	"go_learn/pkg/auth"
 	"go_learn/pkg/response"
 
@@ -20,6 +21,11 @@ func (ctrl *UsersController) CurrentUser(c *gin.Context) {
 
 // Index 所有用户
 func (ctrl *UsersController) Index(c *gin.Context) {
+	request := requests.PaginationRequest{}
+	if ok := requests.Validate(c, &request, requests.Pagination); !ok {
+		return
+	}
+
 	data, pager := user.Paginate(c, 10)
 	response.JSON(c, gin.H{
 		"data":  data,
