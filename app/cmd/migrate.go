679ad74bc73a1d0482ed0a67fa309e4080bb8594
hanbing <hanbing@junohoro.com>
2024-03-05 14:21:05 +0800

    migrate reset 和 fresh 命令

--- app/cmd/migrate.go
+++ app/cmd/migrate.go
@@ -22,6 +22,8 @@ func init() {
 	CmdMigrate.AddCommand(
 		CmdMigrateUp,
 		CmdMigrateRollback,
+		CmdMigrateRest,
+		CmdMigrateRefresh,
 	)
 }
 
@@ -46,4 +48,24 @@ var CmdMigrateRollback = &cobra.Command{
 
 func runDown(cmd *cobra.Command, args []string) {
 	migrator().Rollback()
+}
+
+var CmdMigrateRest = &cobra.Command{
+	Use:   "reset",
+	Short: "Rollback all database migrations",
+	Run:   runReset,
+}
+
+func runReset(cmd *cobra.Command, args []string) {
+	migrator().Reset()
+}
+
+var CmdMigrateRefresh = &cobra.Command{
+	Use:   "refresh",
+	Short: "Reset and re-run all migrations",
+	Run:   runRefresh,
+}
+
+func runRefresh(cmd *cobra.Command, args []string) {
+	migrator().Refresh()
 }
