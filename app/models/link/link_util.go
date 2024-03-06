7049596d25373ab3524650c703ca1787f03c3311
hanbing <hanbing@junohoro.com>
2024-03-06 15:16:52 +0800

    缓存友情链接列表

--- app/models/link/link_util.go
+++ app/models/link/link_util.go
@@ -1,41 +1,62 @@
 package link
 
 import (
-    "go_learn/pkg/app"
-    "go_learn/pkg/database"
-    "go_learn/pkg/paginator"
+	"go_learn/pkg/app"
+	"go_learn/pkg/cache"
+	"go_learn/pkg/database"
+	"go_learn/pkg/helpers"
+	"go_learn/pkg/paginator"
+	"time"
 
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 func Get(idstr string) (link Link) {
-    database.DB.Where("id", idstr).First(&link)
-    return
+	database.DB.Where("id", idstr).First(&link)
+	return
 }
 
 func GetBy(field, value string) (link Link) {
-    database.DB.Where("? = ?", field, value).First(&link)
-    return
+	database.DB.Where("? = ?", field, value).First(&link)
+	return
 }
 
 func All() (links []Link) {
-    database.DB.Find(&links)
-    return
+	database.DB.Find(&links)
+	return
 }
 
 func IsExist(field, value string) bool {
-    var count int64
-    database.DB.Model(Link{}).Where("? = ?", field, value).Count(&count)
-    return count > 0
+	var count int64
+	database.DB.Model(Link{}).Where("? = ?", field, value).Count(&count)
+	return count > 0
 }
 
 func Paginate(c *gin.Context, perPage int) (links []Link, paging paginator.Paging) {
-    paging = paginator.Paginate(
-        c,
-        database.DB.Model(Link{}),
-        &links,
-        app.V1URL(database.TableName(&Link{})),
-        perPage,
-    )
-    return
+	paging = paginator.Paginate(
+		c,
+		database.DB.Model(Link{}),
+		&links,
+		app.V1URL(database.TableName(&Link{})),
+		perPage,
+	)
+	return
+}
+
+func AllCached() (links []Link) {
+	// 设置缓存 key
+	cacheKey := "links:all"
+	// 设置过期时间
+	expireTime := 120 * time.Minute
+	//取数据
+	cache.GetObject(cacheKey, &links)
+	if helpers.Empty(links) {
+		// 查数据库
+		links = All()
+		if !helpers.Empty(links) {
+			// 设置缓存
+			cache.Set(cacheKey, links, expireTime)
+		}
+	}
+	return
 }
